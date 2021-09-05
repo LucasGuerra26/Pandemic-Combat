0740abb8f3adb7ff5ed8c0eeab6ce3ccfa4300d7
Adeildo Pereira <adeildo.pereira@ccc.ufcg.edu.br>
2021-09-05 11:09:38 -0300

    teste

---
+++ pandemic-combat/src/test/java/com/pandemiccombat/pandemiccombat/PandemicCombatApplicationTest.java
@@ -0,0 +1,13 @@
+package com.pandemiccombat.pandemiccombat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemicCombatApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
